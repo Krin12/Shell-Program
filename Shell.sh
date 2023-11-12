4b345ccfedc79cdb3d956d08b455fdb5e51ae937
Kimtaemin <xoals5133@naver.com>
2023-11-12 18:43:32 +0900

    exit을 입력하면 프로그램이 종료되도록 하는 기능구현

--- Shell.sh
+++ Shell.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "Hello, World!"
+echo "This is SHP"
 
+while true; do
+   
+    read -p "명령어를 입력하세요 (끝내려면 'exit' 입력): " userInput
+
+   
+    if [ "$userInput" == "exit" ]; then
+        echo "프로그램을 종료합니다."
+        break
+    else
+
+        echo "입력된 명령어: $userInput"
+        
+    fi
+done
