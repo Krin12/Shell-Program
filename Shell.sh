0420c5ce53740cb70a1d88c288b11b983d238893
Kimtaemin <xoals5133@naver.com>
2023-11-13 16:04:44 +0900

    Ex2, 3

--- Shell.sh
+++ Shell.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "This is SHP"
 
+trap 'echo "Ctrl-Z를 눌러 프로그램을 종료할 수 있습니다."' INT
+trap QUIT
+
+stty -echoctl
+
 while true; do
    
     read -p "명령어를 입력하세요 (끝내려면 'exit' 입력): " userInput
@@ -10,8 +15,15 @@ while true; do
         echo "프로그램을 종료합니다."
         break
     else
-
-        echo "입력된 명령어: $userInput"
         
+        echo "입력된 명령어: $userInput"
+
+        
+        if [[ "$userInput" == *"&"* ]]; then
+            $userInput &
+        else
+       
+            eval "$userInput"
+        fi
     fi
 done
